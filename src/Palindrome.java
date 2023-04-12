150ce571d616d7bfd13338f1cbd9bc8152d91a28
Rajashekar85 <rajjinu4luv@gmail.com>
2023-04-12 12:20:04 +0000

    Modified The copied Files

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,17 +1,20 @@
-    if (inputString.length() == 0) {
-        return true;
-    } else {
-        char firstChar = inputString.charAt(0);
-        char lastChar = inputString.charAt(inputString.length() - 1);
-        String mid = inputString.substring(1, inputString.length() - 1);
-        return (firstChar == lastChar) && isPalindrome(mid);
-    	String rev = "";
-    	int length = inputString.length();
+public class Palindrome {
+	
+	public boolean isPalindrome(String inputString) {
+	    if (inputString.length() == 0) {
+	        return true;
+	    } else {
+	    	String rev = "";
+	    	int length = inputString.length();
+	    	 
 	        for ( int i = length - 1; i >= 0; i-- )
-           rev = rev + inputString.charAt(i);
+	           rev = rev + inputString.charAt(i);
+	   
 	        if (inputString.equals(rev))
 	           return true;
-        else
-           return false;
-    }
+	        else
+	           return false;
+	    }
+	}
+
 }
--- src/PalindromeTest.java
+++ src/PalindromeTest.java
@@ -1,5 +1,3 @@
-package com.training.example.JacocoExample;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
